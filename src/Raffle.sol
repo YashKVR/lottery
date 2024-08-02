343c0d48cbb217926f78df6cb5807ee2ca81820e
YashKVR <yashincrypto5@gmail.com>
2024-08-02 11:07:10 +0530

    Added setUp and basic deployment check test

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -179,5 +179,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
         return i_entranceFee;
     }
 
+    function getRaffleState() external view returns(RaffleState){
+        return s_raffleState;
+    }
+
     
 }
